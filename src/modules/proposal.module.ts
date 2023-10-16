54a444f45915e0dfc3a7b91c82627d6c8675053f
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-10-16 16:18:33 +0530

    feat: add proposal controller and service to proposal module

---
+++ src/modules/proposal.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProposalController } from '../controllers/proposal.controller';
+import { ProposalService } from '../services/proposal.service';
+import { Proposal } from '../entities/proposal.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Proposal])],
+    controllers: [ProposalController],
+    providers: [ProposalService],
+    exports: [ProposalService],
+  })
+  export class ProposalModule {}
