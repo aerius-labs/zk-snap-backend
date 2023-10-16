ec07a5cc7631fd86ad6748e0a54974c58151687b
shryasss <shreyas_londhe@outlook.com>
2023-10-16 12:13:06 +0530

    fix: refactored middleware

--- src/middlewares/dao.middleware.ts
+++ src/middlewares/dao.middleware.ts
@@ -8,36 +8,52 @@ import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { constants } from 'src/constants';
 import { isKeccakHash } from 'src/utils';
-import { NewDaoDto } from 'src/dtos/dao.dto';
 
 @Injectable()
 export class DaoMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.method === 'POST') {
-      this.sanitiseNewDao(req.body);
+      this.sanitiseDaoId(req.body);
+      this.sanitiseDaoName(req.body);
+      this.sanitiseDaoDescription(req.body);
+      this.sanitiseDaoLogo(req.body);
+      this.sanitiseDaoMembersRoot(req.body);
+    } else if (req.method === 'PATCH') {
+      this.sanitiseDaoName(req.body);
+      this.sanitiseDaoDescription(req.body);
+      this.sanitiseDaoLogo(req.body);
+      this.sanitiseDaoMembersRoot(req.body);
     }
     next();
   }
 
-  private sanitiseNewDao(obj: NewDaoDto): void {
+  private sanitiseDaoId(obj: any): void {
     obj.id = uuidv4();
+  }
 
+  private sanitiseDaoName(obj: any): void {
     if (!obj.name || obj.name.length > 50) {
       throw new HttpException(
         'Name should not be empty and not greater than 50 characters',
         HttpStatus.BAD_REQUEST,
       );
     }
+  }
 
+  private sanitiseDaoDescription(obj: any): void {
     if (!obj.description || obj.description.length > 200) {
       throw new HttpException(
         'Description should not be empty and not greater than 200 characters',
         HttpStatus.BAD_REQUEST,
       );
     }
+  }
 
+  private sanitiseDaoLogo(obj: any): void {
     obj.logo = obj.logo || constants.DEFAULT_DAO_IMAGE;
+  }
 
+  private sanitiseDaoMembersRoot(obj: any): void {
     if (!obj.membersRoot || !isKeccakHash(obj.membersRoot)) {
       throw new HttpException(
         'Members root should not be empty and should be a valid keccak256 hash',
