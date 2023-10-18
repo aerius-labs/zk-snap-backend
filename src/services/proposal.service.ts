169c969758f30bd42b6dc655b69592cf500ad503
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-10-18 17:19:02 +0530

    feat: pass key service in proposal service and add public and private key

--- src/services/proposal.service.ts
+++ src/services/proposal.service.ts
@@ -5,17 +5,20 @@ import { InjectRepository } from "@nestjs/typeorm";
 import {Proposal} from '../entities/proposal.entity'
 import {NewProposalDto, UpdateProposalDto} from 'src/dtos/proposal.dto'
 import { DaoService } from "./dao.service";
+import { KeysService } from "./key-gen.sevice";
+import { testnet } from "src/utils/drand-client";
 
 @Injectable()
 export class ProposalService {
     constructor(
         @InjectRepository(Proposal)
         private proposalRepository: Repository<Proposal>,
-        private daoService: DaoService
+        private daoService: DaoService,
+        private keysService: KeysService,
     ) {}
 
     async create(data: NewProposalDto): Promise<Proposal> {
-        const proposal = this.proposalRepository.create(data);
+
         const dao = await this.daoService.findOne(data.dao_id);
         if (!dao) {
             throw new BadRequestException(`Dao with ID ${data.dao_id} does not exist`);
@@ -23,6 +26,25 @@ export class ProposalService {
         if (!dao.members.includes(data.creator)) { 
             throw new BadRequestException(`Creator ${data.creator} is not a member of Dao with ID ${data.dao_id}`);
         }
+        
+        const keys = await this.keysService.generatePallierKeys();
+        const decryptTime = data.end_time.getMilliseconds()
+
+        // Convert BigInt values to strings
+        const objWithStrings = JSON.parse(JSON.stringify(keys.privateKey, (key, value) => typeof value === 'bigint' ? value.toString() : value));
+
+        // Serialize the object to a string
+        const privateString = JSON.stringify(objWithStrings);
+
+        // encrypt the private key
+        const client = testnet();
+        const encrypted = await this.keysService.encrypt(client, privateString, decryptTime);
+
+        let proposal = this.proposalRepository.create(data);
+
+        proposal.encryption_key_pair.public_key = JSON.stringify(keys.publicKey)
+        proposal.encryption_key_pair.private_key = encrypted
+
         try {
             return await this.proposalRepository.save(proposal);
         } catch (error) {
