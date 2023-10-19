acbfb1b4c48b5ebd2e2cf02cdef1fd9b25ce3f11
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-10-19 16:51:48 +0530

    feat: make api to get only members count

--- src/controllers/dao.controller.ts
+++ src/controllers/dao.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { DaoService } from '../services/dao.service';
 import { NewDaoDto, UpdateDaoDto } from 'src/dtos/dao.dto';
+import { Dao } from 'src/entities/dao.entity';
 
 @Controller('dao')
 export class DaoController {
@@ -17,6 +18,18 @@ export class DaoController {
   @Post()
   async create(@Body() createDaoDto: NewDaoDto) {
     return this.daoService.create(createDaoDto);
+  }
+
+  @Get('info')
+  async findInfoAllDao() {
+    const daos = await this.daoService.findAll();
+    const transformedDaos = daos.map((dao: Dao) => ({
+      id: dao.id,
+      name: dao.name,
+      logo: dao.logo,
+      membersCount: Array.isArray(dao.members) ? dao.members.length : 0
+    })); 
+    return transformedDaos;
   }
 
   @Get()
