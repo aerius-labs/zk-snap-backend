07af2f706abaf56b00f8ac78cf47c23f3f49cb5f
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-11-04 09:51:31 +0530

    fix: add class validators in proposal DTOs

--- src/dtos/proposal.dto.ts
+++ src/dtos/proposal.dto.ts
@@ -1,18 +1,76 @@
-export interface NewProposalDto {
-  id?: string;
+import { Transform } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsString,
+  Max,
+  ValidateIf,
+} from 'class-validator';
+export class createdProposalDto {
+  @IsNotEmpty()
+  @IsString()
   creator: string;
+
+  @IsString()
+  @ValidateIf((o) => o.title && o.title.length <= 100, {
+    message: 'Title should not be empty and not greater than 100 characters',
+  })
   title: string;
+
+  @IsString()
+  @ValidateIf((o) => o.description && o.description.length <= 500, {
+    message:
+      'Description should not be empty and not greater than 500 characters',
+  })
   description: string;
+
+  @IsNotEmpty()
+  @IsString()
   dao_id: string;
+
+  @IsNotEmpty()
+  @IsDate()
   start_time: Date;
+
+  @IsNotEmpty()
+  @IsDate()
   end_time: Date;
+
+  @IsNotEmpty()
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayUnique()
+  @Transform(({ value }) => value || ['Yes', 'No'], { toClassOnly: true })
   voting_options: string[];
 }
-
-export interface UpdateProposalDto {
+export class UpdateProposalDto {
+  @IsString()
+  @ValidateIf((o) => o.title && o.title.length <= 100, {
+    message: 'Title should not be empty and not greater than 100 characters',
+  })
   title: string;
+
+  @IsString()
+  @ValidateIf((o) => o.description && o.description.length <= 500, {
+    message:
+      'Description should not be empty and not greater than 500 characters',
+  })
   description: string;
+
+  @IsNotEmpty()
+  @IsDate()
   start_time: Date;
+
+  @IsNotEmpty()
+  @IsDate()
   end_time: Date;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayUnique()
+  @Transform(({ value }) => value || ['Yes', 'No'], { toClassOnly: true })
   voting_options: string[];
 }
