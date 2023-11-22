59d5ad3d00efdd76170fc2a65ea9cca62d236fa6
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-11-22 17:54:56 +0530

    feat: add worker file

--- src/Workers/proposal.worker.ts
+++ src/Workers/proposal.worker.ts
@@ -1,11 +1,12 @@
-
 const { parentPort } = require('worker_threads');
 
 parentPort.on('message', (message) => {
     console.log('message', message);
-    if (message === 'PROPOSAL_CREATED') {
+    if (message.type === 'PROPOSAL_CREATED') {       
         console.log(`Proposal created ${message.value} message from worker`);
+        parentPort.postMessage('PROPOSAL_CREATED');
     }
+
     if (message === 'PROPOSAL_UPDATED') {
         console.log('Proposal updated message from worker');
     }
