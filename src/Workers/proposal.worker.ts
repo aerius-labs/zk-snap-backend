98b0a6b1e38a457bd3b5a0ef4c0c6e40ff2109a5
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-11-24 15:44:55 +0530

    feat: add post to aggregator in the worker on proposal creation

--- src/Workers/proposal.worker.ts
+++ src/Workers/proposal.worker.ts
@@ -1,16 +1,19 @@
+import { workerData } from 'worker_threads';
+import { generateAggregatorBaseProof } from './ProofGeneration/aggregatorBaseProofs';
+
 const { parentPort } = require('worker_threads');
 
 parentPort.on('message', (message) => {
-    console.log('message', message);
-    if (message.type === 'PROPOSAL_CREATED') {       
-        console.log(`Proposal created ${message.value} message from worker`);
-        parentPort.postMessage('PROPOSAL_CREATED');
-    }
+  console.log('message', message);
+  if (message.type === 'PROPOSAL_CREATED') {
+    console.log(`Proposal created ${message.value} message from worker`);
+    generateAggregatorBaseProof(message.value, workerData.aggregatorURL);
+  }
 
-    if (message === 'PROPOSAL_UPDATED') {
-        console.log('Proposal updated message from worker');
-    }
-    if (message === 'USER_VOTED') {
-        console.log('User voted message from worker');
-    }
+  if (message === 'PROPOSAL_UPDATED') {
+    console.log('Proposal updated message from worker');
+  }
+  if (message === 'USER_VOTED') {
+    console.log('User voted message from worker');
+  }
 });
