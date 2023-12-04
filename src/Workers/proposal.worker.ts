6f3b2f48ccd9904af42da7af33b4e505d754337c
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2023-12-04 16:13:41 +0530

    "feat: add aggregate vote and post on aggregator in worker"

--- src/Workers/proposal.worker.ts
+++ src/Workers/proposal.worker.ts
@@ -1,42 +1,46 @@
 import { workerData } from 'worker_threads';
-import { generateAggregatorBaseProof } from './ProofGeneration/aggregatorBaseProofs';
+import { generateAggregatorBaseProof, postAggregatorData } from './ProofGeneration/aggregatorBaseProofs';
 
 const { parentPort } = require('worker_threads');
 import { RabbitMQService } from '../services/rabbitmq.service';
+import { aggregateVote } from 'src/utils/generate-witness';
 
 // Initialize the RabbitMQService
 const rabbitMQService = new RabbitMQService();
-rabbitMQService.onModuleInit().then(() => {
+rabbitMQService
+  .onModuleInit()
+  .then(() => {
     console.log('RabbitMQ Worker: Connected to RabbitMQ');
-}).catch(err => {
+  })
+  .catch((err) => {
     console.error('RabbitMQ Worker: Failed to connect to RabbitMQ', err);
     process.exit(1); // Exit if connection fails
-});
+  });
 
-
-parentPort.on('message', (message) => {
+parentPort.on('message', async (message) => {
   console.log('message', message);
   if (message.type === 'PROPOSAL_CREATED') {
     console.log(`Proposal created ${message.value} message from worker`);
     generateAggregatorBaseProof(message.value, workerData.aggregatorURL);
   }
-
   if (message === 'PROPOSAL_UPDATED') {
     console.log('Proposal updated message from worker');
   }
   if (message.type === 'USER_VOTED') {
+    const { proposalData, vote, zkProof } = message.value;
 
-    const { voteProof, proposalId } = message.data;
     try {
-      rabbitMQService.sendToQueue({
-        voteProof,
-        proposalId,
-      });
-      console.log('vote sent to queue');
+      const witness = await aggregateVote(proposalData, vote, zkProof);
+
+      postAggregatorData(
+        witness,
+        proposalData.proposalIdStr,
+        workerData.aggregatorURL,
+        'recursive',
+      );
     } catch (error) {
-        console.error('Error while sending vote to queue: ', error);
+      console.error('Error while witness generation: ', error);
     }
     console.log('User voted message from worker');
-
   }
 });
